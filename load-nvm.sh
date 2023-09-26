2aab5707475634775a5f35b177285d1fe1281f6b
Sean Zellmer <sean@lejeunerenard.com>
2023-09-26 14:01:30 -0500

    fix: Load nvm from `.nvm/` if not installed via homebrew

--- load-nvm.sh
+++ load-nvm.sh
@@ -1,7 +1,12 @@
 function load-nvm () {
   if [[ $OSTYPE == "darwin"* ]]; then
     export NVM_DIR=$HOME/.nvm
-    [[ -s $(brew --prefix nvm)/nvm.sh ]] && source $(brew --prefix nvm)/nvm.sh
+    BREWFIX=$(brew --prefix nvm)/nvm.sh
+    if [[ -s $BREWFIX ]]; then
+      source $BREWFIX
+    else
+      [[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"
+    fi
   else
     [[ -s "$HOME/.nvm/nvm.sh" ]] && source "$HOME/.nvm/nvm.sh"
   fi
