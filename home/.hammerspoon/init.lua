7ead87960380684528c37a76c633228d00c3d9b8
Sean Zellmer <sean@lejeunerenard.com>
2024-11-10 16:57:10 -0600

    feat: Add window layout for playing rocket league

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -10,3 +10,16 @@ spoon.SpoonInstall.repos.ShiftIt = {
 
 spoon.SpoonInstall:andUse("ShiftIt", { repo = "ShiftIt" })
 spoon.ShiftIt:bindHotkeys({})
+
+-- Layouts
+mainScreen = 'Cinema HD'
+
+function layoutRocketLeague (sessionName)
+  local screen = mainScreen
+  local windowLayout = {
+    {"Brave", "Live Tracker - Rocket League Tracker - Brave", screen, { x = 0.5, y = 0, w = 0.5, h = 0.5 }, nil, nil},
+    {"Brave", sessionName, screen, { x = 0, y = 0, w = 0.5, h = 1 }, nil, nil},
+    {"iTerm", nil, screen, { x = 0.5, y = 0.5, w = 0.5, h = 0.5 }, nil, nil}
+  }
+  instantlayoutApply(windowLayout, plainTextMatch)
+end
